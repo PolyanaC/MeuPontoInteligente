dc5e8ea92ae840384ff888cf2efd219d54f5ab9f
Polyana <polyanacsousa@gmail.com>
2021-03-28 12:18:00 -0300

    Comit inicial dos arquivos do projeto

---
+++ src/test/java/com/meudominio/meupontointeligente/api/MeuPontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meudominio.meupontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
